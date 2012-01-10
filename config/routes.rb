28fa857cc95ea2f80300aa3d65108c7ab0d9ab6d
Colin Van Dyke <colin@everfi.com>
2012-01-10 09:19:28 -0500

    Some layout of the show org screen

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Bridge::Application.routes.draw do
     collection do
       get "map"
     end
+
+    member do
+      get "research"
+      get "volunteer"
+      get "services"
+    end
   end
 
   devise_for :users
